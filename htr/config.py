4b3d43cd3867e4e69f290c377358109290fe8288
Anjali Sarawgi <anjali.sarawgi9@gmail.com>
2026-07-25 14:44:25 +0200

    updated requirements

--- htr/config.py
+++ htr/config.py
@@ -1,7 +1,5 @@
 OCR_MODEL_PATH =  "AnjaliSarawgi/model-fullset-batch4" #"AnjaliSarawgi/model-fullset-57k"
-NUM_BEAMS = 1  # recommended 5
-MAX_LEN = 128 # recommended 256
-
-
+NUM_BEAMS = 1  # recommended 5, min = 1
+MAX_LEN = 128 # recommended 256, min = 128
 TRIE_MIN_LEN = 3 # recommended 3
 Y_TOLERANCE  = 25 # recommended 25
