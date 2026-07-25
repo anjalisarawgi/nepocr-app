c9c8da0cf96ac004777dd23eb5f694c2178bfaaf
Anjali Sarawgi <41316743+anjalisarawgi@users.noreply.github.com>
2026-07-25 18:29:58 +0200

    Update OCR model parameters for better performance

--- htr/config.py
+++ htr/config.py
@@ -1,5 +1,5 @@
 OCR_MODEL_PATH =  "AnjaliSarawgi/model-fullset-batch4" #"AnjaliSarawgi/model-fullset-57k"
-NUM_BEAMS = 1  # recommended 5, min = 1
-MAX_LEN = 128 # recommended 256, min = 128
+NUM_BEAMS = 5  # recommended 5, min = 1
+MAX_LEN = 256 # recommended 256, min = 128
 TRIE_MIN_LEN = 3 # recommended 3
 Y_TOLERANCE  = 25 # recommended 25
